4966ee4ca72835f4e49bde3f5ed76cd486424131
Niklas Mellgren <B441723@prod.sitad.dk>
2025-02-18 11:14:40 +0100

    Removed incompatible package: audioop-lts

--- app.py
+++ app.py
@@ -47,5 +47,4 @@ def chat():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = 8000
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
